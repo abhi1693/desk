3876aa7cc2bd27cda75bfef2bff1ea0bd32dcb93
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2016-06-16 20:14:56 +0530

    Initial commit

--- desk/app/Desk.php
+++ desk/app/Desk.php
@@ -1,7 +1,21 @@
 <?php
+
+use desk\app\web\Application;
+
 /**
- * Created by PhpStorm.
- * User: abhimanyu
- * Date: 16/6/16
- * Time: 2:41 PM
+ * Desk is helper class serving common Desk and Yii2 framework functionality.
+ *
+ * It encapsulates [[Yii]] and ultimately [[YiiBase]], which provides the actual implementation.
+ *
+ * @author Abhimanyu Saharan
  */
+class Desk extends Yii
+{
+	// Editions
+	const Starter = 0;
+
+	/** @var  Application The application instance */
+	public static $app;
+}
+
+spl_autoload_register(['Desk', 'autoload'], TRUE, TRUE);
---
+++ desk/app/web/Application.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace desk\app\web;
+
+use yii\web\Application as WebApplication;
+
+/**
+ * Desk Web Application Class
+ *
+ * @author Abhimanyu Saharan
+ */
+class Application extends WebApplication
+{
+    
+}
